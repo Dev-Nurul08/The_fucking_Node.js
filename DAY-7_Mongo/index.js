144b69ed4edeb768a63adb0ea7a96da17cdd9be4
Dev-Nurul08 <shaikhnurul8200@gmail.com>
2026-01-26 10:45:55 +0530

    working on server to use in mongo db

--- DAY-7_Mongo/index.js
+++ DAY-7_Mongo/index.js
@@ -4,6 +4,7 @@ import {env} from "./config/dotenv.js"
 
 
 const app = express();
+const users = [];
 
 const port = env.PORT || 3000;
 
@@ -14,6 +15,23 @@ app.get('/', (req, res) => {
   return res.render('index');
 });
 
+app.post('/', (req, res) => {
+    const user = {
+        email : req.body.email,
+        password : req.body.password,
+        id : Date.now()
+    }
+    users.push(user);
+    console.table(users);
+    return res.redirect('/');
+});
+
+app.get('/view-data', (req, res) => {
+    if (users.length === 0) {
+        return res.render('pages/view-data', { data: [] }); // Render with empty data
+    }
+    res.render('pages/view-data', { data: users });
+});
 
 
 
